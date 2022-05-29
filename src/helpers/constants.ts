840433992a2651f5104fcce1d0e1e011f60ced5f
Gaël Thomas <gael.thomas@outlook.fr>
2022-05-30 00:58:35 +0700

    feat: add github link

--- src/helpers/constants.ts
+++ src/helpers/constants.ts
@@ -4,4 +4,4 @@ export const CONTRACT_ADDRESS = '0xA02a64a7041a77861f213937449F77875c55D2E7';
 export const CONTRACT_ABI = abi.abi;
 
 export const TWITTER_LINK = 'https://twitter.com/gaelgthomas';
-export const GITHUB_LINK = '';
+export const GITHUB_LINK = 'https://github.com/gaelgthomas/wave-nation';
