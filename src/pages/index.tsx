a1eac6fa8b81b74e1fa2b0e2fd1defacc3bbfafa
Gaël Thomas <22935875+gaelgthomas@users.noreply.github.com>
2022-05-29 11:04:05 +0200

    Initial commit

---
+++ src/pages/index.tsx
@@ -0,0 +1,39 @@
+import * as React from 'react';
+
+import Layout from '@/components/layout/Layout';
+import Seo from '@/components/Seo';
+
+/**
+ * SVGR Support
+ * Caveat: No React Props Type.
+ *
+ * You can override the next-env if the type is important to you
+ * @see https://stackoverflow.com/questions/68103844/how-to-override-next-js-svg-module-declaration
+ */
+
+export default function HomePage() {
+  return (
+    <Layout>
+      {/* <Seo templateTitle='Home' /> */}
+      <Seo />
+
+      <main>
+        <section className='bg-white'>
+          <div className='layout flex min-h-screen flex-col items-center justify-center text-center'>
+            <h1 className='mt-4'>
+              Next.js + Tailwind CSS + TypeScript Starter
+            </h1>
+            <p className='mt-2 text-sm text-gray-800'>
+              A starter for Next.js, Tailwind CSS, and TypeScript with Absolute
+              Import, Seo, Link component, pre-configured with Husky{' '}
+            </p>
+
+            <footer className='absolute bottom-2 text-gray-700'>
+              © {new Date().getFullYear()} By Gaël Thomas
+            </footer>
+          </div>
+        </section>
+      </main>
+    </Layout>
+  );
+}
